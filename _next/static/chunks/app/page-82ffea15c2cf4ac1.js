bc3d49fe3714f709a0d8051bb7903ccd41ed19c3
Light0777 <lightyagami7502@gmail.com>
2025-09-19 08:01:16 +0530

    reviews scetion removed

---
+++ _next/static/chunks/app/page-82ffea15c2cf4ac1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{7394:(e,s,_)=>{Promise.resolve().then(_.bind(_,120))}},e=>{e.O(0,[711,831,512,367,413,592,891,835,120,441,255,358],()=>e(e.s=7394)),_N_E=e.O()}]);
--- _next/static/chunks/app/page-945dd4c33e872350.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{7394:(e,s,_)=>{Promise.resolve().then(_.bind(_,2544))}},e=>{e.O(0,[711,831,512,367,413,592,891,835,544,441,255,358],()=>e(e.s=7394)),_N_E=e.O()}]);
